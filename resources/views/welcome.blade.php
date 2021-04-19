619fa11991384c99e0d8ec0389027822aa81dff9
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-04-19 12:46:17 +0200

    images table with Model and relationship with Users table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<<<<<<< HEAD
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,42 +31,6 @@
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-=======
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Nunito';
-            background: #f7fafc;
-        }
-    </style>
-</head>
-<body>
-    <div class="container-fluid fixed-top p-4">
-        <div class="col-12">
-            <div class="d-flex justify-content-end">
-                @if (Route::has('login'))
-                    <div class="">
-                        @auth
-                            <a href="{{ url('/dashboard') }}" class="text-muted">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-muted">Log in</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-muted">Register</a>
-                            @endif
->>>>>>> 5c9b38ba438f818f5e500c0250bc8655249a616d
                         @endif
                     </div>
                 @endif
