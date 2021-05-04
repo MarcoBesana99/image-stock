798488fcbe070afef492c77e8e5c16ddf2b6b992
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-05-04 18:38:11 +0200

    fix issue of comments

--- app/Http/Livewire/CommentHandler.php
+++ app/Http/Livewire/CommentHandler.php
@@ -10,11 +10,7 @@ class CommentHandler extends Component
 {
     public $comment;
     public $imageId;
-    public $comments;
     public $amount = 1;
-    public $numberOfComments;
-    public $showMore = true;
-    public $showLess = false;
 
     protected $rules = [
         'comment' => 'required'
@@ -22,32 +18,24 @@ class CommentHandler extends Component
 
     public function render()
     {
-        return view('livewire.comment-handler');
-    }
-
-    public function mount() {
-        $this->loadComments();
-        $this->numberOfComments = Comment::where('image_id',$this->imageId)->count();
-        $this->checkLoadingStatus();
-    }
-
-    public function checkLoadingStatus() {
-        if ($this->amount >= $this->numberOfComments) {
-            $this->showMore = false;
-            $this->showLess = True;
+        $allComments = Comment::where('image_id', $this->imageId);
+        $comments = $allComments->orderBy('created_at', 'desc')->take($this->amount)->get();
+        $numberOfComments = $allComments->count();
+        $showMore = true;
+        $showLess = false;
+        if ($this->amount >= $numberOfComments) {
+            $showMore = false;
+            $showLess = True;
         }
         if ($this->amount == 2) {
-            $this->showMore = true;
-            $this->showLess = false;
+            $showMore = true;
+            $showLess = false;
         }
-        if ($this->numberOfComments <=2) {
-            $this->showMore = false;
-            $this->showLess = false;
+        if ($numberOfComments < 2) {
+            $showMore = false;
+            $showLess = false;
         }
-    }
-
-    public function loadComments() {
-        $this->comments =  Comment::where('image_id',$this->imageId)->orderBy('created_at', 'desc')->take($this->amount)->get();
+        return view('livewire.comment-handler',compact('showMore','showLess','comments','numberOfComments'));
     }
 
     public function comment() {
@@ -58,20 +46,15 @@ class CommentHandler extends Component
         $comment->image_id = $this->imageId;
         $comment->save();
         $this->comment = '';
-        $this->emitUp('refreshComments');
     }
 
     public function showMore()
     {
         $this->amount += 2;
-        $this->loadComments();
-        $this->checkLoadingStatus();
     }
 
     public function showLess()
     {
-        $this->amount -= 2;
-        $this->loadComments();
-        $this->checkLoadingStatus();
+        $this->amount = 1;
     }
 }
