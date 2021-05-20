30f397f7cf6664c274b47af69dfc561c9199678f
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-05-20 17:34:36 +0200

    fix upload image

--- app/Http/Livewire/ImageForm.php
+++ app/Http/Livewire/ImageForm.php
@@ -42,10 +42,14 @@ class ImageForm extends Component
             'title' => 'required'
         ]);
 
-        $file = $this->file->store('images', 'public');
+        //$file = $this->file->store('images', 'public');
         $user_id = Auth::user()->id;
 
-        $request['image_path'] = $file;
+        $image = $request->file('images');
+        $name = $image->getClientOriginalName();
+        $image->move(public_path() . '/img/', $name);
+
+        $request['image_path'] = $image;
         $request['tags'] = $tags;
         $request['user_id'] = $user_id;
         $request['title'] = $this->title;
