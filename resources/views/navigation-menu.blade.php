b745316432346d3adb3cbaba8775beae00a9a399
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-05-20 16:26:17 +0200

    fix merge

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -29,7 +29,6 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto align-items-baseline">
-<<<<<<< HEAD
                 @if (Route::has('login'))
                     @if (!Auth::check())
                         <a href="{{ route('login') }}" class="text-sm mr-4 text-gray-700 underline">Log in</a>
@@ -39,8 +38,6 @@
                     @endif
                 @endif
 
-=======
->>>>>>> 49d58d7acb932959976d0e528320d661326b922f
                 <!-- Teams Dropdown -->
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <x-jet-dropdown id="teamManagementDropdown">
